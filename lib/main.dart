be5d1f5d8301ef75403a9b5482cfb047889a43f5
vagnermaltauro <vagnermaltauro@gmail.com>
2022-04-05 22:23:43 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+import "screens/calculator.dart";
+import "../components/keyboard.dart";
+
+void main() {
+  runApp(Calculator());
+}
---
+++ lib/screens/calculator.dart
@@ -0,0 +1,28 @@
+import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/components/models/memory.dart';
+import "package:flutter/material.dart";
+import "../components/display.dart";
+import "../components/keyboard.dart";
+
+class Calculator extends StatefulWidget {
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Column(
+      children: <Widget>[Display(memory.value), Keyboard(_onPressed)],
+    ));
+  }
+}
